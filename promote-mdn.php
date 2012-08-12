2adac07adbbea6b95c834f9bb099a478e8db0627
groovecoder <luke.crouch@gmail.com>
2012-08-12 18:09:57 -0400

    tabs and more cleanup

--- promote-mdn.php
+++ promote-mdn.php
@@ -12,44 +12,43 @@ Description: Promote MDN automatically links keywords phrases to MDN docs
 if ( !class_exists( 'PromoteMDN' ) ) :
 
 class PromoteMDN {
-	var $PromoteMDN_DB_option = 'PromoteMDN';
-	var $PromoteMDN_options; 
-	
-	// Initialize WordPress hooks
-	function PromoteMDN() {	
-        add_filter( 'the_content' ,  array( &$this, 'promote_mdn_the_content_filter' ), 10 );	
+    var $PromoteMDN_DB_option = 'PromoteMDN';
+    var $PromoteMDN_options;
+
+    // Initialize WordPress hooks
+    function PromoteMDN()
+    {
+        add_filter( 'the_content' ,  array( &$this, 'promote_mdn_the_content_filter' ), 10 );
         // Add Options Page
         add_action( 'admin_menu' ,  array( &$this, 'promote_mdn_admin_menu' ) );
-	}
+    }
 
 
     function promote_mdn_process_text( $text )
     {
-        global $wpdb, $post;
         $options = $this->get_options();
         $links   = 0;
         if ( is_feed() && !$options['allowfeed'] )
-             return $text;
-            
+            return $text;
+
         $arrignorepost = $this->explode_trim( ',' , ( $options['ignorepost'] ) );
         if ( is_page( $arrignorepost ) || is_single( $arrignorepost ) ) {
             return $text;
         }
-        
-        $maxlinks     = ( $options['maxlinks'] > 0 ) ? $options['maxlinks'] : 0;	
+
+        $maxlinks     = ( $options['maxlinks'] > 0 ) ? $options['maxlinks'] : 0;
         $maxsingle    = ( $options['maxsingle'] > 0 ) ? $options['maxsingle'] : -1;
         $maxsingleurl = ( $options['maxsingleurl'] > 0 ) ? $options['maxsingleurl'] : 0;
-        $minusage     = ( $options['minusage'] > 0 ) ? $options['minusage'] : 1;
 
         $urls = array();
-            
+
         $arrignore = $this->explode_trim( ',' , ( $options['ignore'] ) );
         if ( $options['excludeheading'] == 'on' ) {
             //Here insert special characters
             $text = preg_replace( '%(<h.*?>)(.*?)(</h.*?>)%sie' , "'\\1'.wp_insertspecialchars('\\2').'\\3'" , $text );
         }
-        
-        $reg_post = '/(?!(?:[^<\[]+[>\]]|[^>\]]+<\/a>))($name)/imsU';	
+
+        $reg_post = '/(?!(?:[^<\[]+[>\]]|[^>\]]+<\/a>))($name)/imsU';
         $reg      = '/(?!(?:[^<\[]+[>\]]|[^>\]]+<\/a>))\b($name)\b/imsU';
         $text     = " $text ";
 
@@ -63,7 +62,7 @@ class PromoteMDN {
             $options['customkey'] = $options['customkey'] . "\n" . $customkey_url_value;
         }
         // custom keywords
-        if ( !empty( $options['customkey'] ) ) {		
+        if ( !empty( $options['customkey'] ) ) {
             $kw_array = array();
             foreach ( explode( "\n" , $options['customkey'] ) as $line ) {
                 $chunks = array_map( 'trim', explode( ',' , $line ) );
@@ -80,7 +79,7 @@ class PromoteMDN {
                         if ( !empty( $keyword ) ) $kw_array[$keyword] = $url;
                 }
             }
-            foreach ( $kw_array as $name => $url ) 
+            foreach ( $kw_array as $name => $url )
             {
                 if (   ( !$maxlinks || ( $links < $maxlinks ) )
                     && ( trailingslashit( $url ) != $thisurl )
@@ -89,24 +88,24 @@ class PromoteMDN {
                    if (
                        ( $options['customkey_preventduplicatelink'] == TRUE ) || stripos( $text, $name ) !== false ) {
                         $name = preg_quote( $name, '/' );
-                        
+
                         if( $options['customkey_preventduplicatelink'] == TRUE ) $name = str_replace( ',' , '|' , $name );
-                        
+
                         $replace = "<a title=\"$1\" href=\"$url\">$1</a>";
-                        $regexp  = str_replace( '$name', $name, $reg );	
+                        $regexp  = str_replace( '$name', $name, $reg );
                         //$regexp="/(?!(?:[^<]+>|[^>]+<\/a>))(?<!\p{L})($name)(?!\p{L})/imsU";
-                        $newtext = preg_replace( $regexp, $replace, $text, $maxsingle );			
-                        if ( $newtext != $text ) {							
+                        $newtext = preg_replace( $regexp, $replace, $text, $maxsingle );
+                        if ( $newtext != $text ) {
                             $links++;
                             $text = $newtext;
                             if ( !isset( $urls[$url] ) ) $urls[$url] = 1; else $urls[$url]++;
-                        }	
+                        }
                     }
-                }		
+                }
             }
         }
-        
-        
+
+
         if ( $options['excludeheading'] == 'on' ) {
             //Here insert special characters
             $text = preg_replace( '%(<h.*?>)(.*?)(</h.*?>)%sie', "'\\1'.wp_removespecialchars('\\2').'\\3'", $text );
@@ -114,43 +113,42 @@ class PromoteMDN {
         }
         return trim( $text );
 
-    } 
+    }
 
-    function promote_mdn_the_content_filter( $text ) {
-        
+    function promote_mdn_the_content_filter( $text )
+    {
         $result  = $this->promote_mdn_process_text( $text );
         $options = $this->get_options();
         $link    = parse_url( get_bloginfo( 'wpurl' ) );
         $host    = 'http://' . $link['host'];
-        
+
         if ( $options['blanko'] )
             $result = preg_replace( '%<a(\s+.*?href=\S(?!' . $host . '))%i', '<a target="_blank"\\1', $result );
-        
+
         return $result;
     }
-	
+
     function explode_trim( $separator, $text )
     {
         $arr = explode( $separator, $text );
-        
+
         $ret = array();
         foreach ( $arr as $e )
-        {        
-          $ret[] = trim( $e );        
+        {
+          $ret[] = trim( $e );
         }
         return $ret;
     }
-	
+
     // Handle our options
-    function get_options() {
+    function get_options()
+    {
      $options = array(
-         'page' => 'on',
-         'excludeheading' => 'on', 
-         'ignore' => 'about,', 
-        'ignorepost' => 'contact', 
+         'excludeheading' => 'on',
+         'ignore' => 'about,',
+        'ignorepost' => 'contact',
          'maxlinks' => 3,
          'maxsingle' => 1,
-         'minusage' => 1,
          'customkey' => '',
          'customkey_url' => 'https://developer.mozilla.org/en-US/docs/Template:Promote-MDN?raw=1',
          'customkey_url_expire' => 60 * 60 * 24,
@@ -159,160 +157,156 @@ class PromoteMDN {
          'allowfeed' => '',
          'maxsingleurl' => '1',
          );
-         
+
         $saved = get_option( $this->PromoteMDN_DB_option );
-     
-     
+
+
          if ( !empty( $saved ) ) {
              foreach ( $saved as $key => $option )
                     $options[$key] = $option;
          }
-        
-         if ( $saved != $options )	
+
+         if ( $saved != $options )
             update_option( $this->PromoteMDN_DB_option, $options );
-        
+
          return $options;
     }
 
-	// Set up everything
-	function install() {
-		$PromoteMDN_options = $this->get_options();		
-	}
-	
-	function handle_options() {
-		$options = $this->get_options();
-		if ( isset( $_POST['submitted'] ) ) {
-			check_admin_referer( 'seo-smart-links' );		
-			
-			$options['page'] = $_POST['page'];					
-			$options['pageself'] = $_POST['pageself'];					
-			$options['excludeheading'] = $_POST['excludeheading'];									
-			$options['ignore'] = $_POST['ignore'];	
-			$options['ignorepost'] = $_POST['ignorepost'];					
-			$options['maxlinks'] = (int) $_POST['maxlinks'];					
-			$options['maxsingle'] = (int) $_POST['maxsingle'];					
-			$options['maxsingleurl'] = (int) $_POST['maxsingleurl'];
-			$options['minusage'] = (int) $_POST['minusage'];
-			$options['customkey'] = $_POST['customkey'];	
+    // Set up everything
+    function install()
+    {
+        $PromoteMDN_options = $this->get_options();
+    }
+
+    function handle_options()
+    {
+        $options = $this->get_options();
+        if ( isset( $_POST['submitted'] ) ) {
+            check_admin_referer( 'seo-smart-links' );
+
+            $options['pageself'] = $_POST['pageself'];
+            $options['excludeheading'] = $_POST['excludeheading'];
+            $options['ignore'] = $_POST['ignore'];
+            $options['ignorepost'] = $_POST['ignorepost'];
+            $options['maxlinks'] = (int) $_POST['maxlinks'];
+            $options['maxsingle'] = (int) $_POST['maxsingle'];
+            $options['maxsingleurl'] = (int) $_POST['maxsingleurl'];
+            $options['customkey'] = $_POST['customkey'];
             $options['customkey_url'] = $_POST['customkey_url'];
             $options['customkey_url_expire'] = $_POST['customkey_url_expire'];
-			$options['blankn'] = $_POST['blankn'];	
-			$options['blanko'] = $_POST['blanko'];	
-			$options['allowfeed'] = $_POST['allowfeed'];	
-			
-			update_option( $this->PromoteMDN_DB_option, $options );
-			$this->promote_mdn_delete_cache( 0 );
-			echo '<div class="updated fade"><p>Plugin settings saved.</p></div>';
-		}
+            $options['blankn'] = $_POST['blankn'];
+            $options['blanko'] = $_POST['blanko'];
+            $options['allowfeed'] = $_POST['allowfeed'];
 
-		$action_url = $_SERVER['REQUEST_URI'];	
+            update_option( $this->PromoteMDN_DB_option, $options );
+            $this->promote_mdn_delete_cache( 0 );
+            echo '<div class="updated fade"><p>Plugin settings saved.</p></div>';
+        }
 
-		$page = $options['page'] == 'on' ? 'checked' : '';
-		$pageself = $options['pageself'] == 'on' ? 'checked' : '';
-		$comment = $options['comment'] == 'on' ? 'checked' : '';
-		$excludeheading = $options['excludeheading'] == 'on' ? 'checked' : '';
-		$lposts = $options['lposts'] == 'on' ? 'checked' : '';
-		$lpages = $options['lpages'] == 'on' ? 'checked' : '';
-		$lcats = $options['lcats'] == 'on' ? 'checked' : '';
-		$ltags = $options['ltags'] == 'on' ? 'checked' : '';
-		$ignore = $options['ignore'];
-		$ignorepost = $options['ignorepost'];
-		$maxlinks = $options['maxlinks'];
-		$maxsingle = $options['maxsingle'];
-		$maxsingleurl = $options['maxsingleurl'];
-		$minusage = $options['minusage'];
-		$customkey = stripslashes( $options['customkey'] );
+        $action_url = $_SERVER['REQUEST_URI'];
+
+        $pageself = $options['pageself'] == 'on' ? 'checked' : '';
+        $comment = $options['comment'] == 'on' ? 'checked' : '';
+        $excludeheading = $options['excludeheading'] == 'on' ? 'checked' : '';
+        $lposts = $options['lposts'] == 'on' ? 'checked' : '';
+        $lpages = $options['lpages'] == 'on' ? 'checked' : '';
+        $lcats = $options['lcats'] == 'on' ? 'checked' : '';
+        $ltags = $options['ltags'] == 'on' ? 'checked' : '';
+        $ignore = $options['ignore'];
+        $ignorepost = $options['ignorepost'];
+        $maxlinks = $options['maxlinks'];
+        $maxsingle = $options['maxsingle'];
+        $maxsingleurl = $options['maxsingleurl'];
+        $customkey = stripslashes( $options['customkey'] );
         $customkey_url = stripslashes( $options['customkey_url'] );
         $customkey_url_expire = stripslashes( $options['customkey_url_expire'] );
-		$nofoln = $options['nofoln'] == 'on' ? 'checked' : '';
-		$nofolo = $options['nofolo'] == 'on' ? 'checked' : '';
-		$blankn = $options['blankn'] == 'on' ? 'checked' : '';
-		$blanko = $options['blanko'] == 'on' ? 'checked' : '';
-		$allowfeed = $options['allowfeed'] == 'on' ? 'checked' : '';
+        $nofoln = $options['nofoln'] == 'on' ? 'checked' : '';
+        $nofolo = $options['nofolo'] == 'on' ? 'checked' : '';
+        $blankn = $options['blankn'] == 'on' ? 'checked' : '';
+        $blanko = $options['blanko'] == 'on' ? 'checked' : '';
+        $allowfeed = $options['allowfeed'] == 'on' ? 'checked' : '';
 
-		if ( !is_numeric( $minusage ) ) $minusage = 1;
-		
-		$nonce = wp_create_nonce( 'seo-smart-links' );
-		
-		$imgpath = trailingslashit( get_option( 'siteurl' ) ). 'wp-content/plugins/seo-automatic-links/i';	
-		echo <<<END
+        $nonce = wp_create_nonce( 'seo-smart-links' );
+
+        $imgpath = trailingslashit( get_option( 'siteurl' ) ). 'wp-content/plugins/seo-automatic-links/i';
+        echo <<<END
 
 <div class="wrap" style="">
-	<h2>Promote MDN</h2>
+    <h2>Promote MDN</h2>
 
-	 <div id="mainblock" style="width:710px">
-	 
-		<div class="dbx-content">
-		 	<form name="PromoteMDN" action="$action_url" method="post">
-		 		    <input type="hidden" id="_wpnonce" name="_wpnonce" value="$nonce" />
-					<input type="hidden" name="submitted" value="1" /> 
-					<p>Promote MDN automatically links keywords and phrases in your posts and pages to MDN URLs.</p>
-					
+     <div id="mainblock" style="width:710px">
+
+        <div class="dbx-content">
+            <form name="PromoteMDN" action="$action_url" method="post">
+                    <input type="hidden" id="_wpnonce" name="_wpnonce" value="$nonce" />
+                    <input type="hidden" name="submitted" value="1" />
+                    <p>Promote MDN automatically links keywords and phrases in your posts and pages to MDN URLs.</p>
+
                     <p>Load keywords from URL:<br/>
                     <input type="text" name="customkey_url" size="90" value="$customkey_url" /><br/>
                     <input type="text" name="customkey_url_expire" size="10" value="$customkey_url_expire"/> <label for="customkey_url_expire">Seconds between reloading</label>
                     </p>
-					<input type="checkbox" name="allowfeed" $allowfeed /> <label for="allowfeed">Add links to RSS feeds</label><br/>
-					<input type="checkbox" name="blanko" $blanko /> <label for="blanko">Open links in new window</label> <br/>
-					
-					<h4>Exceptions</h4>				
-					<input type="checkbox" name="excludeheading"  $excludeheading/> <label for="excludeheading">Do not add links in heading tags (h1,h2,h3,h4,h5,h6).</label><br/>
-					<p>Do not add links to the following posts or pages: (comma-separated id, slug, name)</p>
-					<input type="text" name="ignorepost" size="90" value="$ignorepost"/> 
-					<br>
-                    
-					<p>Do not add links on the following phrases: (comma-separated)</p>
-					<input type="text" name="ignore" size="90" value="$ignore"/> 
-					<br><br>                 
-                    
-                    <h4>Limits</h4>				
-					Max links to generate per post: <input type="text" name="maxlinks" size="2" value="$maxlinks"/><br/>
-					Max links to generate for a single keyword/phrase: <input type="text" name="maxsingle" size="2" value="$maxsingle"/><br/>
-					Max links to generate for a single URL: <input type="text" name="maxsingleurl" size="2" value="$maxsingleurl"/>
-					 					 
-					<h4>Custom Keywords</h4>
-					<p>Extra keywords to automaticaly link. Use comma to seperate keywords and add target url at the end. Use a new line for new url and set of keywords. e.g.,<br/>
+                    <input type="checkbox" name="allowfeed" $allowfeed /> <label for="allowfeed">Add links to RSS feeds</label><br/>
+                    <input type="checkbox" name="blanko" $blanko /> <label for="blanko">Open links in new window</label> <br/>
+
+                    <h4>Exceptions</h4>
+                    <input type="checkbox" name="excludeheading"  $excludeheading/> <label for="excludeheading">Do not add links in heading tags (h1,h2,h3,h4,h5,h6).</label><br/>
+                    <p>Do not add links to the following posts or pages: (comma-separated id, slug, name)</p>
+                    <input type="text" name="ignorepost" size="90" value="$ignorepost"/>
+                    <br>
+
+                    <p>Do not add links on the following phrases: (comma-separated)</p>
+                    <input type="text" name="ignore" size="90" value="$ignore"/>
+                    <br><br>
+
+                    <h4>Limits</h4>
+                    Max links to generate per post: <input type="text" name="maxlinks" size="2" value="$maxlinks"/><br/>
+                    Max links to generate for a single keyword/phrase: <input type="text" name="maxsingle" size="2" value="$maxsingle"/><br/>
+                    Max links to generate for a single URL: <input type="text" name="maxsingleurl" size="2" value="$maxsingleurl"/>
+
+                    <h4>Custom Keywords</h4>
+                    <p>Extra keywords to automaticaly link. Use comma to seperate keywords and add target url at the end. Use a new line for new url and set of keywords. e.g.,<br/>
                     <pre>addons, amo, http://addons.mozilla.org/
 support, sumo, http://support.mozilla.org/
                     </pre>
-					</p>
-					
-					<textarea name="customkey" id="customkey" rows="10" cols="90"  >$customkey</textarea>
-					<br><br>
+                    </p>
 
-					<div class="submit"><input type="submit" name="Submit" value="Update options" class="button-primary" /></div>
-			</form>
-		</div>
-		
-		<br/><br/><h3>&nbsp;</h3>	
-	 </div>
+                    <textarea name="customkey" id="customkey" rows="10" cols="90"  >$customkey</textarea>
+                    <br><br>
 
-	</div>
-	
+                    <div class="submit"><input type="submit" name="Submit" value="Update options" class="button-primary" /></div>
+            </form>
+        </div>
+
+        <br/><br/><h3>&nbsp;</h3>
+     </div>
+
+    </div>
+
 </div>
 END;
-		
-		
-	}
-	
-	function promote_mdn_admin_menu()
-	{
-		add_options_page( 'Promote MDN Options', 'Promote MDN', 8, basename( __FILE__ ), array( &$this, 'handle_options' ) );
-	}
 
-    function promote_mdn_delete_cache( $id ) {
+
+    }
+
+    function promote_mdn_admin_menu()
+    {
+        add_options_page( 'Promote MDN Options', 'Promote MDN', 8, basename( __FILE__ ), array( &$this, 'handle_options' ) );
+    }
+
+    function promote_mdn_delete_cache( $id )
+    {
         error_log( 'promote_mdn_delete_cache' );
     }
 }
 
-endif; 
+endif;
 
 if ( class_exists( 'PromoteMDN' ) ) :
-	
-	$PromoteMDN = new PromoteMDN();
-	if ( isset( $PromoteMDN ) ) {
-		register_activation_hook( __FILE__, array( &$PromoteMDN, 'install' ) );
-	}
+    $PromoteMDN = new PromoteMDN();
+    if ( isset( $PromoteMDN ) ) {
+        register_activation_hook( __FILE__, array( &$PromoteMDN, 'install' ) );
+    }
 endif;
 
 function wp_insertspecialchars( $str ) {
