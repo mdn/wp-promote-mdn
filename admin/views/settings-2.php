13cfa672cbb70830a18299195ddac8c4ecb91a6d
Daniele Scasciafratte <mte90net@gmail.com>
2017-10-20 00:03:02 +0200

    added all the fields

--- admin/views/settings-2.php
+++ admin/views/settings-2.php
@@ -1,27 +1,24 @@
 
-		<div id="tabs-2" class="wrap">
-			<?php
-						$cmb = new_cmb2_box( array(
-				'id' => PM_TEXTDOMAIN . '_options-second',
-				'hookup' => false,
-				'show_on' => array( 'key' => 'options-page', 'value' => array( PM_TEXTDOMAIN ), ),
-				'show_names' => true,
-					) );
-			$cmb->add_field( array(
-				'name' => __( 'Text', PM_TEXTDOMAIN ),
-				'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-				'id' => '_text-second',
-				'type' => 'text',
-				'default' => 'Default Text',
+<div id="tabs-2" class="wrap">
+	<?php
+	$cmb = new_cmb2_box( array(
+		'id' => PM_TEXTDOMAIN . '_keywords',
+		'hookup' => false,
+		'show_on' => array( 'key' => 'options-page', 'value' => array( PM_TEXTDOMAIN ), ),
+		'show_names' => true,
 			) );
-			$cmb->add_field( array(
-				'name' => __( 'Color Picker', PM_TEXTDOMAIN ),
-				'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-				'id' => '_colorpicker-second',
-				'type' => 'colorpicker',
-				'default' => '#bada55',
-			) );
-			cmb2_metabox_form( PM_TEXTDOMAIN . '_options-second', PM_TEXTDOMAIN . '-settings-second' );
-						?>
-			<!-- @TODO: Provide other markup for your options page here. -->
-		</div>
+	$cmb->add_field( array(
+		'name' => __( 'Custom Keywords', PM_TEXTDOMAIN ),
+		'id' => 'first_title',
+		'type' => 'title',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Extra keywords to automaticaly link.<br> Use comma to seperate keywords and add target url at the end.<br> Use a new line for new url and set of keywords.', PM_TEXTDOMAIN ),
+		'desc' => __( 'Note: These keywords will take priority over those loaded at the URL. If you have too many custom keywords here, you may not link to MDN at all.', PM_TEXTDOMAIN ),
+		'id' => 'customkey',
+		'before_field' => 'Examples:<br><pre>addons, amo, http://addons.mozilla.org/</pre><pre>sumo, http://support.mozilla.org/</pre>',
+		'type' => 'textarea',
+	) );
+	cmb2_metabox_form( PM_TEXTDOMAIN . '_keywords', PM_TEXTDOMAIN . '-keywords' );
+	?>
+</div>
--- admin/views/settings.php
+++ admin/views/settings.php
@@ -12,131 +12,119 @@
 		'type' => 'title',
 	) );
 	$cmb->add_field( array(
-		'name' => __( 'Text', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => 'text',
-		'type' => 'text',
-		'default' => 'Default Text',
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Test Text Medium', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_textmedium',
-		'type' => 'text_medium',
-			// 'repeatable' => true,
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Website URL', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_url',
+		'name' => __( 'Load keywords from URL', PM_TEXTDOMAIN ),
+		'id' => 'url',
+		'default' => 'https://raw.githubusercontent.com/mdn/wp-promote-mdn/def-list/terms.txt',
 		'type' => 'text_url',
 	) );
 	$cmb->add_field( array(
-		'name' => __( 'Test Text Area', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_textarea',
-		'type' => 'textarea',
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Test Text Area Small', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_textareasmall',
-		'type' => 'textarea_small',
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Test Title Weeeee', PM_TEXTDOMAIN ),
-		'desc' => __( 'This is a title description', PM_TEXTDOMAIN ),
-		'id' => '_title',
-		'type' => 'title',
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Test Select', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_select',
-		'type' => 'select',
-		'show_option_none' => true,
-		'options' => array(
-			'standard' => __( 'Option One', PM_TEXTDOMAIN ),
-			'custom' => __( 'Option Two', PM_TEXTDOMAIN ),
-			'none' => __( 'Option Three', PM_TEXTDOMAIN ),
-		),
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Test Radio inline', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_radio_inline',
+		'name' => __( 'Reload keywords after (seconds)', PM_TEXTDOMAIN ),
+		'id' => 'recurrence',
 		'type' => 'radio_inline',
-		'show_option_none' => 'No Selection',
 		'options' => array(
-			'standard' => __( 'Option One', PM_TEXTDOMAIN ),
-			'custom' => __( 'Option Two', PM_TEXTDOMAIN ),
-			'none' => __( 'Option Three', PM_TEXTDOMAIN ),
+			'hourly' => __( 'Hourly', PM_TEXTDOMAIN ),
+			'daily' => __( 'Daily', PM_TEXTDOMAIN ),
+			'twicedaily' => __( 'Twice daily', PM_TEXTDOMAIN ),
+			'weekly' => __( 'Weekly', PM_TEXTDOMAIN ),
+			'monthly' => __( 'Monthly', PM_TEXTDOMAIN ),
 		),
 	) );
 	$cmb->add_field( array(
-		'name' => __( 'Test Radio', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_radio',
-		'type' => 'radio',
-		'options' => array(
-			'option1' => __( 'Option One', PM_TEXTDOMAIN ),
-			'option2' => __( 'Option Two', PM_TEXTDOMAIN ),
-			'option3' => __( 'Option Three', PM_TEXTDOMAIN ),
-		),
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Test Taxonomy Radio', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_text_taxonomy_radio',
-		'type' => 'taxonomy_radio',
-		'taxonomy' => 'category', // Taxonomy Slug
-			// 'inline'  => true, // Toggles display to inline
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Test Taxonomy Select', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_taxonomy_select',
-		'type' => 'taxonomy_select',
-		'taxonomy' => 'category', // Taxonomy Slug
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Test Taxonomy Multi Checkbox', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_multitaxonomy',
-		'type' => 'taxonomy_multicheck',
-		'taxonomy' => 'category', // Taxonomy Slug
-	) );
-	$cmb->add_field( array(
-		'name' => __( 'Test Checkbox', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_checkbox',
+		'name' => __( 'Add links to RSS feeds', PM_TEXTDOMAIN ),
+		'id' => 'allowfeed',
 		'type' => 'checkbox',
 	) );
 	$cmb->add_field( array(
-		'name' => __( 'Test Multi Checkbox', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_multicheckbox',
+		'name' => __( 'Add links to comments', PM_TEXTDOMAIN ),
+		'id' => 'allowcomments',
+		'type' => 'checkbox',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Include src url params (Helps MDN measure effectiveness)', PM_TEXTDOMAIN ),
+		'id' => 'add_src_param',
+		'type' => 'checkbox',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Open links in new window', PM_TEXTDOMAIN ),
+		'id' => 'blanko',
+		'type' => 'checkbox',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Exceptions', PM_TEXTDOMAIN ),
+		'id' => 'second_title',
+		'type' => 'title',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Do not add links to any <em>pages</em>.', PM_TEXTDOMAIN ),
+		'id' => 'ignoreallpages',
+		'type' => 'checkbox',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Do not add links to any <em>pages</em>.', PM_TEXTDOMAIN ),
+		'id' => 'ignoreallposts',
+		'type' => 'checkbox',
+	) );
+	// Get list of post types
+	$post_types = get_post_types( '', 'names' );
+	unset( $post_types[ 'revision' ] );
+	unset( $post_types[ 'attachment' ] );
+	unset( $post_types[ 'post' ] );
+	unset( $post_types[ 'page' ] );
+	unset( $post_types[ 'nav_menu_item' ] );
+	unset( $post_types[ 'custom_css' ] );
+	unset( $post_types[ 'customize_changeset' ] );
+	unset( $post_types[ 'iwp_log' ] );
+	foreach ( $post_types as $key => $post_type ) {
+		$post_types[ $key ] = sprintf( __( 'Do not add links to any <em>%s</em>.', PM_TEXTDOMAIN ), $post_type );
+	}
+	$cmb->add_field( array(
+		'name' => __( 'Post types', PM_TEXTDOMAIN ),
+		'id' => 'ignoreposttype',
 		'type' => 'multicheck',
-		'options' => array(
-			'check1' => __( 'Check One', PM_TEXTDOMAIN ),
-			'check2' => __( 'Check Two', PM_TEXTDOMAIN ),
-			'check3' => __( 'Check Three', PM_TEXTDOMAIN ),
-		),
+		'options' => $post_types
 	) );
 	$cmb->add_field( array(
-		'name' => __( 'Test wysiwyg', PM_TEXTDOMAIN ),
-		'desc' => __( 'field description (optional)', PM_TEXTDOMAIN ),
-		'id' => '_wysiwyg',
-		'type' => 'wysiwyg',
-		'options' => array( 'textarea_rows' => 5, ),
+		'name' => __( 'Do not add links to the following posts or pages (comma-separated id, slug, name)', PM_TEXTDOMAIN ),
+		'id' => 'ignorepost',
+		'default' => 'contact,',
+		'type' => 'text',
 	) );
 	$cmb->add_field( array(
-		'name' => __( 'Test Image', PM_TEXTDOMAIN ),
-		'desc' => __( 'Upload an image or enter a URL.', PM_TEXTDOMAIN ),
-		'id' => '_image',
-		'type' => 'file',
+		'name' => __( 'Do not add links inside the following HTML elements (comma-separated, partial-matching)', PM_TEXTDOMAIN ),
+		'id' => 'exclude_elems',
+		'default' => 'blockquote, code, h, pre, q',
+		'type' => 'text',
 	) );
-
+	$cmb->add_field( array(
+		'name' => __( 'Do not add links on the following phrases (comma-separated)', PM_TEXTDOMAIN ),
+		'id' => 'ignore',
+		'default' => 'about,',
+		'type' => 'text',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Limits', PM_TEXTDOMAIN ),
+		'id' => 'third_title',
+		'type' => 'title',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Max links to generate per post', PM_TEXTDOMAIN ),
+		'description' => __( '(0 to disable all links)', PM_TEXTDOMAIN ),
+		'id' => 'maxlinks',
+		'default' => '14',
+		'type' => 'text_small',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Max links to generate for a single keyword/phrase', PM_TEXTDOMAIN ),
+		'id' => 'maxsingle',
+		'default' => '1',
+		'type' => 'text_small',
+	) );
+	$cmb->add_field( array(
+		'name' => __( 'Max links to generate for a single URL', PM_TEXTDOMAIN ),
+		'id' => 'maxsingleurl',
+		'default' => '1',
+		'type' => 'text_small',
+	) );
 	cmb2_metabox_form( PM_TEXTDOMAIN . '_options', PM_TEXTDOMAIN . '-settings' );
 	?>
 	<!-- @TODO: Provide other markup for your options page here. -->
