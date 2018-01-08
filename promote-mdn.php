1cc4a79a26f4b25eda244856843827b724f002dd
Daniele Scasciafratte <mte90net@gmail.com>
2018-01-08 15:58:06 +0100

    bump

--- promote-mdn.php
+++ promote-mdn.php
@@ -8,8 +8,8 @@
  *
  * Plugin Name:       Promote MDN
  * Description:       Automatically links your WordPress blog with MDN.
- * Version:           2.0.2
- * Author:            Luke Crouch and Daniele Scasciafratte
+ * Version:           2.0.3
+ * Author:            Daniele Scasciafratte and Luke Crouch
  * Author URI:        https://github.com/mdn/wp-promote-mdn
  * Text Domain:       promote-mdn
  * License:           GPL 2.0+
@@ -21,7 +21,7 @@
 if ( !defined( 'WPINC' ) ) {
 	die;
 }
-define( 'PM_VERSION', '2.0.2' );
+define( 'PM_VERSION', '2.0.3' );
 define( 'PM_TEXTDOMAIN', 'promote-mdn' );
 define( 'PM_NAME', 'Promote MDN' );
 define( 'PM_PLUGIN_ROOT', plugin_dir_path( __FILE__ ) );
